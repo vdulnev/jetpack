26e720b7dc63fd71489e2ee40c07f2495fe9bddb
slavad <vjacheslav.dulnev@privatbank.ua>
2018-11-06 17:49:01 +0200

    1st commit

--- app/src/main/java/com/example/slavad/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/slavad/jetpack/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.slavad.jetpack
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.Navigation
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavHost {
+
+    override fun getNavController(): NavController {
+        return Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
